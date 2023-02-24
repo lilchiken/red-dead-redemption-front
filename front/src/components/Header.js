1cbc768c4a2516d86c17bb712d19e0e9fe3907ca
localmachine <localmachine@Airlocalmachine.Dlink>
2023-02-25 01:52:39 +0300

    start

---
+++ front/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>Red Dead Redemption API</span>
+            <ul className='nav'>
+                <li>About</li>
+                <li>Feedback</li>
+                <li>Chat</li>
+            </ul>
+        </div>
+        <div>
+
+        </div>
+    </header>
+  )
+}
